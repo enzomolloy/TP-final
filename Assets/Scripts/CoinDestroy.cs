c6665fdf6c8220a151e1db5d24da6dabd3da42d4
enzomolloy <enzomolloy@gmail.com>
2022-05-28 00:25:22 -0300

    Player movement & Coin

---
+++ Assets/Scripts/CoinDestroy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinDestroy : MonoBehaviour
+{
+    public GameObject coin;
+    int countercoin;
+    public Text lbl;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(coin);
+            countercoin++;
+          
+            Debug.Log(countercoin);
+        }
+
+        
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int maxJumps;
+    float shift;
+    int hasJump;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shift = (movementSpeed * 2);
+        hasJump = maxJumps;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) && hasJump == maxJumps)
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A) && hasJump == maxJumps)
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && hasJump > 0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump--;
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && hasJump == maxJumps)
+        {
+            transform.Translate(0, 0, shift);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            hasJump = maxJumps;
+        }
+    }
+}
