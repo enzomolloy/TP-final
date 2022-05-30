9f4fa3043f054f11339d420650b0ddc4bbdf35e3
enzomolloy <enzomolloy@gmail.com>
2022-05-29 21:33:00 -0300

    Log launcher

---
+++ Assets/Scripts/Logs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logs : MonoBehaviour
+{
+    public Transform spawnear;
+    public GameObject log;
+    GameObject clone;
+
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+
+    IEnumerator ExampleCoroutine()
+    {
+
+      while(true) 
+        {
+            clone = Instantiate(log, spawnear.transform.position, spawnear.transform.rotation);
+            yield return new WaitForSeconds(3);
+            Destroy(clone, 15);
+        }
+
+
+    }
+}
+
+//https://answers.unity.com/questions/1784952/add-delay-in-while-loop.html
