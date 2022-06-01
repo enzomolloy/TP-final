a50814ef5653b8af549ad94d26691b8de32161c6
enzomolloy <enzomolloy@gmail.com>
2022-05-31 21:24:29 -0300

    Life & Dead

---
+++ Assets/Scripts/Lifecounter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lifecounter : MonoBehaviour
+{
+    public static int countervida = 200;
+    public Text vida;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vida.text = (countervida.ToString());
+    }
+}
---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLife : MonoBehaviour
+{
+    public Text gameOver;
+    public GameObject player;
+    public GameObject camara;
+    
+
+    void Start()
+    {
+        gameOver.enabled = false;
+        
+    }
+    void Update()
+    {
+        if (Lifecounter.countervida > 0)
+        {
+
+        
+         if (transform.position.y < -10)
+         {
+            Lifecounter.countervida -= 100;
+
+            transform.position = new Vector3(0, -5, -30);
+         }
+
+        }
+        else
+        {
+            gameOver.enabled = true;
+            camara.transform.position = new Vector3(100, 100, 100);
+        }
+
+    }
+
+}
+
